eebe095092b816442f6e73ab64fdacca8bfee9f4
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-25 15:56:05 +0300

    Update the root route

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :groups_purchases
-  has_many :purchases, through: :groups_purchases
+  has_many :purchases, through: :groups_purchases, dependent: :destroy
+  has_one_attached :image
 end
