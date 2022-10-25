b59958420ae65449a42a6300b7256729a1e4c42d
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-25 11:17:56 +0300

    Install active storage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users #:controllers => { registrations: 'registrations' }
+  devise_for :users
   resources :groups
   resources :purchases
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#sign_up"
+  root "groups#index"
 end
