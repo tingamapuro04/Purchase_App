8ea08164498eeb1f6bcc77ed82197dfd463becbb
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-26 06:24:00 +0300

    Add Bootstrap

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,5 @@ class Purchase < ApplicationRecord
   belongs_to :user, class_name: 'User'
   has_many :groups_purchases
   has_many :groups, through: :groups_purchases
+  has_one_attached :image
 end
