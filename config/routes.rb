794ff3801f8a3643c7b9960343b7fd0c2719aed5
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-26 21:13:17 +0300

    Add bullet gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   resources :groups
   resources :purchases
   resources :users
+  resources :homes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root 'groups#index'
-  
   root 'groups#index'
 end
