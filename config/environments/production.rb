f22b9f30532ababd4b2ca3b637bd24c0c8a79cd2
tingamapuro04 <alponcemobutu100@gmail.com>
2022-10-27 13:25:29 +0300

    smtp delivery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,21 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'https://naivas-purchase.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'naivas-purchase.herokuapp.com/', protocol: 'https' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'https://naivas-purchase.herokuapp.com/',
+    user_name: 'alponcemobutu100@gmail.com',
+    password: 'xeuunviastrzszff',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -67,7 +81,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
